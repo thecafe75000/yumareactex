19926c7954d0c98ddbe5eaf10cb1572a9690d6b3
LEI <lei@Laptop-LEI-MAC.local>
2024-12-04 22:23:11 +0100

    arrange components of part category

---
+++ src/views/Product/Category/components/CategoryModal.tsx
@@ -0,0 +1,157 @@
+import { useMemo, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Modal, Form, Input, Cascader, InputNumber, Switch } from 'antd'
+import { useAppDispatch, useMessage} from '@/utils'
+import type { TStoreState } from '@/store'
+import { postProductCategoryAsync, putProductCategoryAsync } from '@/store/slice/category'
+
+
+type TProps = {
+  isModalOpen: boolean
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  info: any
+  parentId: string[]
+  modalTitle: string
+  setModalTitle: React.Dispatch<React.SetStateAction<string>>
+}
+
+const CategoryModal = (props: TProps) => {
+  const { isModalOpen, setIsModalOpen, info, parentId, modalTitle, setModalTitle} = props
+  const dispatch = useAppDispatch()
+  const message = useMessage()
+  // 获取表单实例
+  const [form] = Form.useForm()
+
+  useEffect(() => {
+    if (info._id) {
+       form.setFieldValue('sort', info.sort)
+       form.setFieldValue('name', info.name)
+       form.setFieldValue('showFlag', info.showFlag === 1 ? true : false)
+       form.setFieldValue('parentId', info.ids)
+       form.setFieldValue('_id', info._id)
+    } else {
+      form.resetFields()
+    }
+    setModalTitle('Modify product category')
+  }, [info])
+
+  useEffect(() => {
+    if (parentId.length > 0) {
+      form.setFieldValue('parentId', parentId)
+    } else {
+      form.resetFields()
+    }
+    setModalTitle('Add product category')
+  },[parentId])
+
+  const { categoryList } = useSelector((state: TStoreState) => state.category)
+  const parentIdArr = useMemo(() => categoryList.map((item: any) => {
+        let obj: any = {
+          label: item.name,
+          value: item._id
+        }
+        if (item.children) {
+          obj.children = item.children.map((child: any) => {
+            return {
+              label: child.name,
+              value: child._id
+            }
+          })
+        }
+        return obj
+      }),[categoryList])
+     // console.log('parentidarr',parentIdArr)
+
+  const handleOk = () => {
+    // 提交表单
+    form.submit()
+  }
+
+  const handleCancel = () => {
+    // 重置表单内容
+    form.resetFields()
+    setIsModalOpen(false)
+  }
+
+  const onFinish = async (body: any) => {
+    body.showFlag = body.showFlag ? 1 : 0
+    if (Array.isArray(body.parentId)) {
+      body.parentId = body.parentId[body.parentId.length - 1]
+    }
+    // console.log('finish-body',body)
+    // 存在_id就是修改商品分类,不存在就是添加商品分类
+    if (body._id) {
+      await dispatch(putProductCategoryAsync(body))
+    } else {
+      await dispatch(postProductCategoryAsync(body))
+    }
+    // 重置弹出框里表单的内容,即回填数据
+    form.resetFields()
+    setIsModalOpen(false)
+    message.success(modalTitle + 'add new category successfully!')
+  }
+
+  return (
+    <Modal
+      forceRender
+      title={modalTitle}
+      maskClosable={false}
+      open={isModalOpen}
+      // closable={false}
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Form
+        name='basic'
+        // 表单实例
+        form={form}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{
+          sort: 0, // 排序的默认值
+          showFlag: true
+        }}
+        onFinish={(body) => onFinish(body)}
+        autoComplete='off'
+      >
+        <Form.Item name='_id' hidden>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label='Previous Category'
+          // name 的值要看接口文档
+          name='parentId'
+        >
+          <Cascader
+            changeOnSelect
+            options={parentIdArr}
+            onChange={() => {}}
+            placeholder='If this item is omitted, the top-level category is added'
+          />
+        </Form.Item>
+        <Form.Item
+          label='Category Name'
+          // name 的值要看接口文档
+          name='name'
+          rules={[{ required: true, message: 'Please add a category!' }]}
+        >
+          <Input placeholder='Please add a category' />
+        </Form.Item>
+        <Form.Item label='Show or Not' name='showFlag'>
+          <Switch defaultChecked={true} />
+        </Form.Item>
+        <Form.Item
+          label='Sorting'
+          // name 的值要看接口文档
+          name='sort'
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <InputNumber />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default CategoryModal
--- src/views/Product/Category/index.tsx
+++ src/views/Product/Category/index.tsx
@@ -1,16 +1,11 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import {
   Button,
-  Form,
-  Input,
   Space,
   Switch,
   Table,
   Tooltip,
-  Modal,
-  Cascader,
-  InputNumber,
   Popconfirm
 } from 'antd'
 import {
@@ -20,8 +15,9 @@ import {
   PlusOutlined
 } from '@ant-design/icons'
 import { useAppDispatch, useMessage } from '@/utils'
-import { deteleProductCategoryAsync, getProductAllCategoryListAsync, postProductCategoryAsync, putProductCategoryAsync, putProductShowFlagAsync } from '@/store/slice/category'
+import { deteleProductCategoryAsync, getProductAllCategoryListAsync, putProductShowFlagAsync } from '@/store/slice/category'
 import type { TStoreState } from '@/store'
+import CategoryModal from './components/CategoryModal'
 
 
 const Category = () => {
@@ -30,28 +26,11 @@ const Category = () => {
   const { loading } = useSelector((state: TStoreState) => state.config)
   const [key, setKey] = useState('')
   const message = useMessage()
+  const [info, setInfo] = useState({})
+  const [parentId, setParentId] = useState<string[]>([])
+  const [modalTitle, setModalTitle] = useState<string>('Add product category') 
   // 控制对话框modal的显示与隐藏
-  const [isModelOpen, setIsModalOpen] = useState(false)
-  // 获取表单实例
-  const [form] = Form.useForm()
-  const [modalTitle, setModalTitle] = useState('Add product category')
-
-  const parentIdArr = useMemo(() => categoryList.map((item: any) => {
-    let obj:any = {
-      label: item.name,
-      value: item._id
-    }
-    if (item.children) {
-      obj.children = item.children.map((child: any) => {
-        return {
-          label: child.name,
-          value: child._id
-        }
-      })
-    }
-    return obj
-  }), [categoryList])
-  // console.log('parentidarr',parentIdArr)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
     dispatch(getProductAllCategoryListAsync())
@@ -61,7 +40,6 @@ const Category = () => {
     setKey(rows._id)
     await dispatch(putProductShowFlagAsync(rows._id, isChecked ? 1 : 0))
     // 数字2表示message在屏幕上停留的时间为2秒
-    // message.success('put the info of '+ rows.name + ' at '+ (isChecked? 'display':'hide') + ' successfully', 2)
     message.success((
       <>
         <span style={{color:'red'}}>put successfully {rows.name} at {isChecked ? 'display' : 'hide'} </span>
@@ -71,37 +49,9 @@ const Category = () => {
   }
 
   const buttonClick = () => {
-    setModalTitle('Add product category')
+    setInfo({})
+    setParentId([])
     setIsModalOpen(true)
-  }
-
-  const handleOk = () => {
-    // 提交表单
-    form.submit()
-  }
-
-  const handleCancel = () => {
-    // 重置表单内容
-    form.resetFields()
-    setIsModalOpen(false)
-  }
-
-  const onFinish = async (body:any) => {
-    body.showFlag = body.showFlag ? 1 : 0
-    if (Array.isArray(body.parentId)) {
-      body.parentId = body.parentId[body.parentId.length -1]
-    }
-    // console.log('finish-body',body)
-    // 存在_id就是修改商品分类,不存在就是添加商品分类
-    if (body._id) {
-      await dispatch(putProductCategoryAsync(body))
-    } else {
-      await dispatch(postProductCategoryAsync(body))
-    }
-    // 重置弹出框里表单的内容,即回填数据
-    form.resetFields()
-    setIsModalOpen(false)
-    message.success( modalTitle + 'successfully!')
   }
 
   return (
@@ -137,6 +87,8 @@ const Category = () => {
             title: 'Show or Not',
             dataIndex: 'showFlag',
             align: 'center',
+            // render函数第一个参数就是dataIndex的值,
+            // 如果没有dataIndex, 第1个参数则是rows,是表单当前行整行的所有数据
             render(showFlag, rows: any) {
               return (
                 // 第1种写法
@@ -170,27 +122,20 @@ const Category = () => {
                     <Button
                       onClick={() => {
                         //  console.log('button-add rows', rows)
-                        form.setFieldValue('parentId', [...rows.ids, rows._id])
-                        setModalTitle('Add product category')
+                        // form.setFieldValue('parentId', [...rows.ids, rows._id])
+                        // setModalTitle('Add product category')
+                        setParentId([...rows.ids, rows._id])
                         setIsModalOpen(true)
                       }}
                       icon={<FileAddOutlined />}
                     >
-                      + a new subcategory
+                      Add new subcategory
                     </Button>
                   )}
                   <Button
                     onClick={() => {
-                      form.setFieldValue('sort', rows.sort)
-                      form.setFieldValue('name', rows.name)
-                      form.setFieldValue(
-                        'showFlag',
-                        rows.showFlag === 1 ? true : false
-                      )
-                      form.setFieldValue('parentId', rows.ids)
-                      form.setFieldValue('_id', rows._id)
+                      setInfo(rows)
                       setIsModalOpen(true)
-                      setModalTitle('Modify product category')
                     }}
                     icon={<EditOutlined />}
                     type='primary'
@@ -223,64 +168,14 @@ const Category = () => {
         rowKey='_id'
         dataSource={categoryList}
       />
-      <Modal
-        title={modalTitle}
-        maskClosable={false}
-        open={isModelOpen}
-        // closable={false}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Form
-          name='basic'
-          // 表单实例
-          form={form}
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{
-            sort: 0, // 排序的默认值
-            showFlag: true
-          }}
-          onFinish={(body) => onFinish(body)}
-          autoComplete='off'
-        >
-          <Form.Item name='_id' hidden>
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label='Previous Category'
-            // name 的值要看接口文档
-            name='parentId'
-          >
-            <Cascader
-              changeOnSelect
-              options={parentIdArr}
-              onChange={() => {}}
-              placeholder='If this item is omitted, the top-level category is added'
-            />
-          </Form.Item>
-          <Form.Item
-            label='Category Name'
-            // name 的值要看接口文档
-            name='name'
-            rules={[{ required: true, message: 'Please add a category!' }]}
-          >
-            <Input placeholder='Please add a category' />
-          </Form.Item>
-          <Form.Item label='Show or Not' name='showFlag'>
-            <Switch defaultChecked={true} />
-          </Form.Item>
-          <Form.Item
-            label='Sorting'
-            // name 的值要看接口文档
-            name='sort'
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <InputNumber />
-          </Form.Item>
-        </Form>
-      </Modal>
+      <CategoryModal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        info={info}
+        parentId={parentId}
+        modalTitle={modalTitle}
+        setModalTitle={setModalTitle}
+      />
     </Space>
   )
 }
